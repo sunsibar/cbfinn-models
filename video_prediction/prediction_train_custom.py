95efd76c514336eb93e4f4fe0c9f5e8991002580
su-si <susanne.h.keller@gmail.com>
2018-10-15 10:41:27 +0200

    only store best after 15000 iters

--- video_prediction/prediction_train_custom.py
+++ video_prediction/prediction_train_custom.py
@@ -306,7 +306,7 @@ def main(unused_argv):
     if (itr) % SAVE_INTERVAL == 2:
       tf.logging.info('Saving model.')
       saver.save(sess, FLAGS.output_dir + '/model' + str(itr))
-    if val_loss < lowest_loss and itr >= 10000: # 10000: should depend on the value of schedsamp-k, but am too lazy to do
+    if val_loss < lowest_loss and itr >= 15000: # 10000: should depend on the value of schedsamp-k, but am too lazy to do
                                 # that right now. Ignore good values in the area where a lot of ground truth data is used.
         best_save_path = os.path.join(FLAGS.output_dir, 'best_weights')
         best_save_path = saver_best.save(sess, best_save_path, global_step=itr + 1)
