ffdf6103623556507666238fbf684e8b37cd48f3
su-si <susanne.h.keller@gmail.com>
2018-10-17 09:08:05 +0200

    its inputs, not input

--- video_prediction/prediction_utils_custom.py
+++ video_prediction/prediction_utils_custom.py
@@ -81,7 +81,7 @@ class Model(object):
     self.prediction = self.gen_images = gen_images
     enc_dict =  {'enc'+str(i): tf.stack(self.core_model.encs[i], axis=1) for i in range(len(self.core_model.encs))}
     hidd_dict = {'hidden'+str(i+1): tf.stack(self.core_model.hidden_layers[i], axis=1) for i in range(len(self.core_model.hidden_layers))}
-    self.layers = {**hidd_dict, **enc_dict, 'input': tf.stack(images, axis=1)}
+    self.layers = {**hidd_dict, **enc_dict, 'inputs': tf.stack(images, axis=1)}
     self.layer_names = self.layers.keys()
     self.bottleneck_layers = self.layers # todo?
     self.bottleneck_layer_names = self.bottleneck_layers.keys()
